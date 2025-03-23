d36e5338147392213fdb544fbd439b4b0e3ecce6
jmirja <shohan.mirja@gmail.com>
2025-03-24 03:21:06 +0600

    Adding Movie Service

---
+++ MovieApi/Services/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using MovieManager.DTOs;
+
+namespace MovieManager.Services
+{
+    public interface IMovieService
+    {
+        Task<MovieDto> CreateMovieAsync(CreateMovieDto command);
+        Task<MovieDto?> GetMovieByIdAsync(Guid id);
+        Task<IEnumerable<MovieDto>> GetAllMoviesAsync();
+        Task UpdateMovieAsync(Guid id, UpdateMovieDto command);
+        Task DeleteMovieAsync(Guid id);
+    }
+}
